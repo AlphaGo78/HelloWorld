a320205b3aa1e7b6125245d694b17fcb256fbafc
yangpeihao <yangpeihao999@gmail.com>
2024-05-29 22:21:47 +0800

    no message

--- demo01/CarLoan.java
+++ demo01/CarLoan.java
@@ -1,2 +1,27 @@
-package demo01;public class CarLoan {
+package demo01;
+
+public class CarLoan {
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+
+    public CarLoan(int carLoan, int loanLength, int interestRate, int downPayment){
+        if(loanLength <= 0 || interestRate <= 0){
+            System.out.println("Error! You must take out a valid car loan.");
+        } else if(downPayment > carLoan){
+            System.out.println("The car can be paid in full.");
+        } else{
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength*12;
+            int monthlyBalance = remainingBalance/months;
+            int interest = interestRate * monthlyBalance;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
+        }
+    }
+
+    public static void main(String[] args) {
+        CarLoan carLoan1 = new CarLoan(10000,3,5,2000);
+    }
 }
--- demo01/Order.java
+++ demo01/Order.java
@@ -1,2 +1,53 @@
-package demo01;public class Order {
+package demo01;
+
+public class Order {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+
+    public Order(boolean filled, double cost, String shippingMethod) {
+        if(cost > 24.00){
+            System.out.println("High value item!");
+        } else {
+            System.out.println("Low value item!");
+        }
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+    }
+
+    public void ship(){
+        if(isFilled){
+            System.out.println("Shipping");
+        } else {
+            System.out.println("Order not ready");
+        }
+
+        double shippingCost = calculateShipping();
+
+        System.out.println("Shipping cost: " + shippingCost);
+    }
+
+    public double calculateShipping(){
+        double shippingCost;
+        switch(shipping){
+            case "Regular":
+                shippingCost = 0;
+                break;
+            case "Express":
+                shippingCost = 1.75;
+                break;
+            default:
+                shippingCost = .50;
+        }
+        return shippingCost;
+    }
+
+    public static void main(String[] args) {
+        Order o1 = new Order(true,25.00,"Regular");
+        o1.ship();
+        Order o2 = new Order(false,23.00,"Express");
+        o2.ship();
+    }
+
 }
